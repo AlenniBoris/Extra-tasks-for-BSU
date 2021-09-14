418d460732d69eb362e4a76db3a25074c7a270d3
Boris Alennikov <AlennikovBoris@gmail.com>
2021-09-14 13:55:26 +0300

    Quadratnoe uravnenie
    Solution

---
+++ quadratnoe uravnenie/cmake-build-debug/CMakeFiles/quadratnoe_uravnenie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadratnoe_uravnenie.dir/main.cpp.obj"
+  "quadratnoe_uravnenie.exe"
+  "quadratnoe_uravnenie.exe.manifest"
+  "quadratnoe_uravnenie.lib"
+  "quadratnoe_uravnenie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadratnoe_uravnenie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ quadratnoe uravnenie/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    double x1, x2 , d;
+
+
+    cout  << "Enter a:";
+    cin >> a;
+    cout  << "Enter b:";
+    cin >> b;
+    cout  << "Enter c:";
+    cin >> c;
+
+    d= sqrt(pow(b,2)-(4*a*c));
+
+    if (d<0){
+        cout << "There is no solution";
+    }
+    if (d==0){
+        x1= (-b)/2*a;
+        cout << "Your solution:";
+        cout << x1;
+    }
+    if (d>0){
+        x1= ((-b)-d)/2*a;
+        x2= ((-b)+d)/2*a;
+        cout << "Your solution:";
+        cout << x1 <<endl;
+        cout << x2;
+    }
+
+
+}
